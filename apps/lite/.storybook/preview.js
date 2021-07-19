b55ba591aac283ebe12b8cca3418a70551e8d424
Elise Chant <elise.chant@gmail.com>
2021-07-20 05:21:58 +1000

    aubody and augrid classes

--- apps/lite/.storybook/preview.js
+++ apps/lite/.storybook/preview.js
@@ -1,6 +1,8 @@
 import '../src/auds.tokens';
 import '../src/auds';
 
+import { addDecorator } from "@storybook/html";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -10,3 +12,8 @@ export const parameters = {
     },
   },
 }
+
+addDecorator(story => {
+  document.body.classList.add('au-body', 'au-grid');
+  return story();
+})
